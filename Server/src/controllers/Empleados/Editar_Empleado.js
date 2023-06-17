b8514ad4ac5761d8d873d5592f89730838b9749b
DarkKevo <kevinaraujogonzalez@gmail.com>
2023-06-16 23:44:39 -0400

    Agregado el CRUD de empleados, falta la documentacion y la ruta del mismo (voy pa esa)

---
+++ Server/src/controllers/Empleados/Editar_Empleado.js
@@ -0,0 +1,92 @@
+import mysql from 'mysql2';
+
+import { host, port, username, password } from '../../Config/MySqlConfig.js';
+
+export const EditarEmpleado = (req, res) => {
+  var conexion = mysql.createConnection({
+    host: host,
+    port: port,
+    user: username,
+    password: password,
+    multipleStatements: true,
+  });
+
+  conexion.connect(function (err) {
+    if (err) {
+      console.error('Error de conexion: ' + err.stack);
+      return;
+    }
+  });
+
+  const {
+    idEmpleados,
+    cedula,
+    nombres,
+    apellidos,
+    fecha_nacimiento,
+    direccion,
+    correo,
+    codigo_cargo,
+    codigo_departamento,
+    codigo_deduccion,
+    codigo_empresa,
+    estado,
+  } = req.body;
+
+  let verify = 'SELECT * FROM nomina_database.cargos where `idEmpleados`= ' + `'${idEmpleados}'`;
+
+  let update =
+    'UPDATE nomina_database.empleados SET `cedula`= ' +
+    `'${cedula}'` +
+    ', `nombres`= ' +
+    `'${nombres}'` +
+    +', `apellidos`= ' +
+    `'${apellidos}'` +
+    +', `fecha_nacimiento`= ' +
+    `'${fecha_nacimiento}'` +
+    +', `direccion`= ' +
+    `'${direccion}'` +
+    +', `correo`= ' +
+    `'${correo}'` +
+    +', `codigo_cargo`= ' +
+    `'${codigo_cargo}'` +
+    +', `codigo_departamento`= ' +
+    `'${codigo_departamento}'` +
+    +', `codigo_deduccion`= ' +
+    `'${codigo_deduccion}'` +
+    +', `codigo_empresa`= ' +
+    `'${codigo_empresa}'` +
+    +', `estado`= ' +
+    `'${estado}'` +
+    'where `idEmpleados`= ' +
+    `'${idEmpleados}'`;
+
+  //Verificando la existencia del empleado
+  conexion.query(verify, (err, result) => {
+    if (err) {
+      console.log(err);
+      conexion.end();
+      res.sendStatus(400);
+    } else {
+      if (result.length != 0) {
+        //Actualizando el empleado
+        conexion.query(update, (err, results) => {
+          if (err) {
+            console.log(err);
+            conexion.end();
+            res.sendStatus(400);
+          } else {
+            //Empleado Actualizado
+            console.log(results);
+            conexion.end();
+            res.sendStatus(200);
+          }
+        });
+      } else {
+        console.log('No existe el empleado');
+        conexion.end();
+        res.sendStatus(400);
+      }
+    }
+  });
+};
---
+++ Server/src/controllers/Empleados/Registrar_Empleado.js
@@ -0,0 +1,68 @@
+import mysql from 'mysql2';
+
+import { host, port, username, password } from '../../Config/MySqlConfig.js';
+
+export const Crear_Empleado = (req, res) => {
+  var conexion = mysql.createConnection({
+    host: host,
+    port: port,
+    user: username,
+    password: password,
+    multipleStatements: true,
+  });
+
+  conexion.connect(function (err) {
+    if (err) {
+      console.error('Error de conexion: ' + err.stack);
+      return;
+    }
+  });
+
+  const {
+    cedula,
+    nombres,
+    apellidos,
+    fecha_nacimiento,
+    direccion,
+    correo,
+    codigo_cargo,
+    codigo_departamento,
+    codigo_deduccion,
+    codigo_empresa,
+    estado,
+  } = req.body;
+
+  let verify = 'SELECT * FROM nomina_database.empleados where `cedula`= ' + `'${cedula}'`;
+
+  let query =
+    'INSERT INTO `nomina_database`.`empleados` (`cedula`, `nombres`, `apellidos, `fecha_nacimiento`, `direccion`, `correo`, `codigo_cargo`, `codigo_departamento`, `codigo_deduccion`, `codigo_empresa`, `estado`,) VALUES ';
+
+  query += `('${cedula}', '${nombres}', '${apellidos}', '${fecha_nacimiento}', '${direccion}', '${correo}', '${codigo_cargo}', '${codigo_departamento}', '${codigo_deduccion}', '${codigo_empresa}', '${estado}',)`;
+
+  //Verificar la Existencia del Empleado
+  conexion.query(verify, (err, result) => {
+    if (err) {
+      console.log(err);
+      conexion.end();
+      res.sendStatus(400);
+    } else {
+      if (result.length == 0) {
+        conexion.query(query, (err, results) => {
+          if (err) {
+            console.log(err);
+            conexion.end();
+            res.sendStatus(400);
+          } else {
+            console.log(results);
+            conexion.end();
+            res.sendStatus(200);
+          }
+        });
+      } else {
+        console.log('Ya existe el cargo');
+        conexion.end();
+        res.sendStatus(400);
+      }
+    }
+  });
+};
