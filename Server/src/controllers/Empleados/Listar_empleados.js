96d58b51b6601d29fa797c79da335dbafd1c6162
Yetzenia Mendoza <126374259+YetzeniaM7@users.noreply.github.com>
2023-07-13 22:24:43 -0400

    Update Listar Empleados y Buscar Empleados
    Update Controllers Listar Empleados y Buscar Empleados
    Para mostrar los datos correspondientes de acuerdo a las tablas relacionadas.

--- Server/src/controllers/Empleados/Listar_empleados.js
+++ Server/src/controllers/Empleados/Listar_empleados.js
@@ -18,7 +18,25 @@ export const ListarEmpleado = (req, res) => {
     }
   });
 
-  let query = 'SELECT * FROM nomina_database.empleados';
+  let query = `SELECT E.idEmpleados,
+	E.cedula cedula,
+	E.nombres nombres,
+	E.apellidos apellidos,
+	E.fecha_nacimiento fecha_nacimiento,
+	E.direccion direccion,
+	E.correo correo,
+	E.telefono telefono,
+	c.cargo codigo_cargo,
+	d.departamento codigo_departamento,
+	em.nombre codigo_empresa, 
+	E.estado estado
+FROM nomina_database.empleados E,
+nomina_database.cargos c,
+nomina_database.departamentos d,
+nomina_database.empresas EM
+where c.idcargos = e.codigo_cargo
+and d.iddepartamentos = e.codigo_departamento
+and em.idEmpresas = e.codigo_empresa`;
 
   //Verificando la existencia de los empleados
   conexion.query(query, (err, result) => {
