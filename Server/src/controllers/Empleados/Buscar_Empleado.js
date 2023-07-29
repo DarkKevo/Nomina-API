b47c7f9ded4bf21a7f6b9db2b34327a90cdbdda3
JMCA2805 <126287724+JMCA2805@users.noreply.github.com>
2023-07-29 19:26:11 -0400

    fixed

--- Server/src/controllers/Empleados/Buscar_Empleado.js
+++ Server/src/controllers/Empleados/Buscar_Empleado.js
@@ -21,21 +21,28 @@ export const BuscarEmpleado = (req, res) => {
   const { idEmpleados } = req.body;
 
   let query =
-    `SELECT idEmpleados,
-	cedula,
-	nombres,
-  apellidos,
-  fecha_nacimiento,
-  direccion,
-  correo,
-  telefono,
-  codigo_cargo,
-  codigo_departamento,
-  codigo_empresa, 
-  estado,
-  numero_cuenta,
-  antiguedad 
-  FROM nomina_database.empleados where idEmpleados= ${idEmpleados}`
+  `SELECT E.idEmpleados,
+	E.cedula,
+	E.nombres,
+	E.apellidos,
+	E.fecha_nacimiento,
+	E.direccion,
+	E.correo,
+	E.telefono,
+	c.cargo codigo_cargo,
+	d.departamento codigo_departamento,
+	em.nombre codigo_empresa, 
+	E.estado,
+  E.numero_cuenta,
+  E.antiguedad
+FROM nomina_database.empleados E,
+nomina_database.cargos c,
+nomina_database.departamentos d,
+nomina_database.empresas EM
+where c.idcargos = e.codigo_cargo
+and d.iddepartamentos = e.codigo_departamento
+and em.idEmpresas = e.codigo_empresa
+and e.idEmpleados =` + idEmpleados;
 
   //Verificando la existencia de los empleados
   conexion.query(query, (err, result) => {
--- Server/src/controllers/Empleados/Listar_empleados.js
+++ Server/src/controllers/Empleados/Listar_empleados.js
@@ -19,18 +19,18 @@ export const ListarEmpleado = (req, res) => {
   });
 
   let query =  `SELECT E.idEmpleados,
-	E.cedula cedula,
-	E.nombres nombres,
-	E.apellidos apellidos,
-	E.fecha_nacimiento fecha_nacimiento,
-	E.correo correo,
-	E.telefono telefono,
+	E.cedula,
+	E.nombres,
+	E.apellidos,
+	E.fecha_nacimiento,
+	E.correo,
+	E.telefono,
 	c.cargo codigo_cargo,
 	d.departamento codigo_departamento,
 	em.nombre codigo_empresa, 
-  E.numero_cuenta numero_cuenta,
-	E.antiguedad antiguedad,
-	E.estado estado
+  E.numero_cuenta,
+	E.antiguedad,
+	E.estado
 FROM nomina_database.empleados E,
 nomina_database.cargos c,
 nomina_database.departamentos d,
