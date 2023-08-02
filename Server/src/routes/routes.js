395719b67fcc7bb7fa549fab0f94166f8b7f0661
JMCA2805 <126287724+JMCA2805@users.noreply.github.com>
2023-08-02 00:21:38 -0400

    Actualizando rutas

--- Server/src/routes/routes.js
+++ Server/src/routes/routes.js
@@ -5,6 +5,7 @@ import { GenerarPagos } from '../controllers/Planilla_Pago/GenerarPagos.js';
 
 //Cargar Horas
 import { CargarHoras } from '../controllers/Carga_Horas/Cargar_horas.js';
+import { ListarHoras } from '../controllers/Carga_Horas/Listar_Horas.js';
 
 //Registros
 import { ListarRegistro } from '../controllers/Registro_De_Horas/ListarRegistro.js';
@@ -65,6 +66,7 @@ export const routes = Express.Router();
 
 //Cargar horas
 routes.post('/cargarHoras', CargarHoras);
+routes.get('/listarHoras', ListarHoras);
 
 //Usuarios
 routes.post('/newUser', newUser);
